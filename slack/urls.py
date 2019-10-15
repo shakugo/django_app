5bdfd0c171de2e33d4f58aa1ba9e0c0978409cc1
Keisuke Fukuda <comet.helping@gmail.com>
2019-10-15 21:08:17 +0900

    imple login

--- slack/urls.py
+++ slack/urls.py
@@ -5,4 +5,5 @@ app_name = 'slack'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:room_name>/', views.room, name='room'),
+    path('login', views.Account_login.as_view(), name='login'),
 ]
--- slack/views.py
+++ slack/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from django.utils.safestring import mark_safe
 import json
 
+from django.views.generic import View
 # Create your views here.
 def index(request):
     return render(request, 'slack/index.html', {})
@@ -14,3 +15,20 @@ def room(request, room_name):
     return render(request, 'slack/room.html', {
         'room_name_json': mark_safe(json.dumps(room_name))
     })
+
+#ログイン機能
+class Account_login(View):
+    def post(self, request, *arg, **kwargs):
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            user = User.objects.get(username=username)
+            login(request, user)
+            return redirect('/')
+        return render(request, 'login.html', {'form': form,})
+
+    def get(self, request, *args, **kwargs):
+        form = LoginForm(request.POST)
+        return render(request, 'login.html', {'form': form,})
+
+account_login = Account_login.as_view()
